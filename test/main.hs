5aa6d9f6dd3a95529f22e48b80752cea7d97bfb8
Michael Snoyman <michael@snoyman.com>
2012-11-22 10:59:26 +0200

    Test suite runs

--- test/main.hs
+++ test/main.hs
@@ -5,7 +5,10 @@ import FileLocation
 import Control.Exception.Base (SomeException, Exception(..))
 import Prelude hiding (catch)
 import Control.Exception.Lifted (catch)
+import Control.Monad (unless)
 
+import System.Environment (getArgs)
+import System.Process (readProcessWithExitCode)
 
 
 
@@ -17,6 +20,35 @@ instance Exception AException
 
 
 main = do
+    args <- getArgs
+    case args of
+        [] -> do
+            (_, stdout, stderr) <- readProcessWithExitCode "dist/build/test/test" ["foo"] ""
+            shelltest <- readFile "test/file-location.shelltest"
+            let (stdout', stderr') = parseShellTest shelltest
+            unless (unlines (lines stdout) == stdout') $ do
+                putStrLn "Invalid stdout:"
+                putStr stdout
+                error "Failure"
+            unless (unlines (lines stderr) == stderr') $ do
+                putStrLn "Invalid stderr:"
+                putStr stderr
+                error "Failure"
+            putStrLn "Success"
+        _ -> main2
+
+
+parseShellTest :: String -> (String, String)
+parseShellTest orig =
+    (unlines stdout, unlines stderr)
+  where
+    ls1 = lines orig
+    ls2 = drop 1 $ dropWhile (/= ">>>") ls1
+    (stdout, ls3) = break (== ">>>2") ls2
+    stderr = takeWhile (/= ">>>= 1") $ drop 1 ls3
+
+main2 :: IO ()
+main2 = do
   let _ = debugMsgIf "Not Visble" id False
   let x = debugMsgIf "debugMsgIf" (\xs -> head xs == 1) [1,2,3]
   putStrLn . show $ $(dbgMsg "Msg TH") $ debugMsg "Msg plain" $ $(dbg) $ debug $ $(trc "trc") x
