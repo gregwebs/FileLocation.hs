b7a6238e63a3951e24227f7561280e46bb09403a
Greg Weber <greg@gregweber.info>
2015-07-01 11:23:58 -0700

    change assertEq to just operate in IO
    Also add `assertEq` which uses MonadIO
    Do the same for `assertB`

--- Test/FileLocation.hs
+++ Test/FileLocation.hs
@@ -7,20 +7,29 @@ import FileLocation (locationToString)
 import Control.Monad.IO.Class (liftIO)
 
 -- | A version of assertEqual that gives location information.
---
--- TODO: push upstream to file-location
 assertEq :: Q Exp
 assertEq = do
   loc <- location
   let prefix = locationToString loc ++ " "
-  [|(\x -> liftIO . HUnit.assertEqual prefix x)|]
+  [|(\x -> HUnit.assertEqual prefix x)|]
 
 -- | a MonadIO version of assertBool that gives location information.
---
--- TODO: push upstream to file-location
 assertB :: Q Exp
 assertB = do
   loc <- location
   let prefix = locationToString loc ++ "assertB "
-  [|(liftIO . HUnit.assertBool prefix)|]
+  [|(HUnit.assertBool prefix)|]
 
+-- | Same as 'assertEq', but uses 'liftIO'
+assertEq' :: Q Exp
+assertEq' = do
+  loc <- location
+  let prefix = locationToString loc ++ " "
+  [|(\x -> liftIO . HUnit.assertEqual prefix x)|]
+
+-- | Same as 'assertB, but uses 'liftIO'
+assertB' :: Q Exp
+assertB' = do
+  loc <- location
+  let prefix = locationToString loc ++ "assertB "
+  [|(liftIO . HUnit.assertBool prefix)|]
